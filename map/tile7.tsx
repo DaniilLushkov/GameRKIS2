1da70a024a8a0fd97e37868327c04f2fa9ac928f
DaniilLushkov <150797253+DaniilLushkov@users.noreply.github.com>
2024-06-10 11:48:41 +0300

    Add files via upload

---
+++ map/tile7.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="tile7" tilewidth="16" tileheight="16" tilecount="96" columns="12">
+ <image source="../../Desktop/gameRKIS-main/1_Industrial_Tileset_1(1).png" width="192" height="128"/>
+</tileset>
