6b6a98000e70179cd9ad5c081998cd26bd56aa6f
DaniilLushkov <150797253+DaniilLushkov@users.noreply.github.com>
2024-06-11 18:13:13 +0300

    Add files via upload

---
+++ map/tile10.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="tile10" tilewidth="16" tileheight="16" tilecount="4320" columns="54">
+ <image source="../tile/tile10/GandalfHardcore FREE Platformer Assets/GandalfHardcore Platformer Assets.png" width="864" height="1280"/>
+</tileset>
---
+++ map/tile9.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="tile9" tilewidth="16" tileheight="16" tilecount="384" columns="24">
+ <image source="../tile/tile9/free.png" width="384" height="256"/>
+</tileset>
