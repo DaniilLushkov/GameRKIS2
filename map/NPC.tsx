94824e24e8f9ec4d44264ed7be86717b2df261cf
DaniilLushkov <150797253+DaniilLushkov@users.noreply.github.com>
2024-06-13 18:32:54 +0300

    Add files via upload

--- map/NPC.tsx
+++ map/NPC.tsx
@@ -1,2 +1,16 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="NPC" tilewidth="1" tileheight="1" tilecount="0" columns="0"/>
+<tileset version="1.10" tiledversion="1.10.2" name="NPC" tilewidth="130" tileheight="48" tilecount="4" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="../tile/npc/Sprite/WithoutWeapon/Idle/IdleWithoutWeapon.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="../tile/npc/Sprite/MainCharacter/Idle/Idle.png"/>
+ </tile>
+ <tile id="2" x="0" y="0" width="130" height="44">
+  <image width="138" height="47" source="../tile/npc/notNPC/image-Ge8UTwqe--transformed.png"/>
+ </tile>
+ <tile id="4">
+  <image width="48" height="48" source="../tile/npc/Sprite/WithoutWeapon/Idle/image-nDgfFS_MD-transformed (1).png"/>
+ </tile>
+</tileset>
--- map/tile6.tsx
+++ map/tile6.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="tile6" tilewidth="16" tileheight="16" tilecount="100" columns="20">
- <image source="../tile/tile6/00Atlas.png" width="320" height="80"/>
+<tileset version="1.10" tiledversion="1.10.2" name="tile6" tilewidth="16" tileheight="16" tilecount="400" columns="20">
+ <image source="../tile/tile6/00Atlas (1).png" width="320" height="80"/>
 </tileset>
